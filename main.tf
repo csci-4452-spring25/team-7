34548ea45b09f5e34241c48b6d653b3da94e6b7f
sgusain2004 <129971172+sgusain2004@users.noreply.github.com>
2025-04-30 23:08:00 -0500

    spun terra

--- main.tf
+++ main.tf
@@ -1,23 +1,40 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-# basic example using ami lookup https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-#configuration for AWS provider
-provider "aws" {
-  region = "us-east-1"
+  tags = {
+    Name = "Public Subnet"
+  }
 }
 
-# setting up a security group to allow http traffic and allow all outgoing
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "app_sg" {
+  vpc_id      = aws_vpc.main.id
   name        = "app-security-group"
-  description = "Allow HTTP inbound traffic and all outbound traffic"
+  description = "Allow HTTP and SSH access"
 
   ingress {
     description = "Allow HTTP from anywhere"
@@ -36,7 +53,7 @@ resource "aws_security_group" "app_sg" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -45,12 +62,13 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-0e449927258d45bc4" #published 04/11/2025
-  instance_type          = "t2.micro"              # Free tier eligible // for now 
-  key_name               = "my_ec2_key"
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  ami                         = "ami-0e449927258d45bc4"
+  instance_type               = "t2.micro"
+  key_name                    = "my_ec2_key"
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
 
-  # User data: Install Docker and run container
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
